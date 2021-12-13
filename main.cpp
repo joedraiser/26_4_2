15e556e3ebf8238fdbebc6181b93549fd3b7d831
Evgenii.Korchagov <joedraiser@gmail.com>
2021-12-13 10:40:57 +0300

    Done

--- main.cpp
+++ main.cpp
@@ -1,61 +1,122 @@
 #include <iostream>
+#include <vector>
 
 class phoneNumber
 {
 private:
     std::string name;
-    std::string number
-    int qty=0;
+    std::string number;
 public:
+    phoneNumber()
+    {
+    }
+
     phoneNumber(std::string name, std::string number)
     {
         this->name=name;
-        pNums[0]=new int[10];
-        for(int i=0;i<10;i++)
-        {
-            *(pNums[0]+i)=number[i];
-        }
-        qty++;
+        this->number=number;
     }
 
-    void addNumber(int number[10])
+    std::string getName()
     {
-        pNums[qty]=new int[10];
-        for(int i=0;i<10;i++)
-        {
-            *(pNums[qty]+i)=number[i];
-        }
-        qty++;
+        return name;
     }
 
-    void showName()
+    std::string getNumber()
     {
-        std::cout << name;
-    }
-
-    int getQty()
-    {
-        return qty;
-    }
-
-    int* getNumber(int n)
-    {
-        return pNums[n];
+        return number;
     }
 };
 
-class phone
+class phoneList
 {
 private:
-    phoneNumber contacts;
+    std::vector<phoneNumber> contacts;
 public:
+    phoneList()
+    {
+    }
 
+    void add()
+    {
+        std::string name, number;
+        std:: cout << "Input name: ";
+        std::cin >> name;
+        while(true)
+        {
+            std::cout << "Input number: ";
+            std::cin >> number;
+            if(number.length()!=12)
+                std::cout << "Not valid number\n";
+            else if(number[0]!='+'||number[1]!='7')
+                std::cout << "Number doesnt start with +7\n";
+            else if (number.substr(2,10)!=std::to_string(std::stoll(number.substr(2,10))) )
+                std::cout << "Invalid number\n";
+            else
+                break;
+        }
+        contacts.push_back(phoneNumber (name, number));
+    }
+
+    void call()
+    {
+        std::string input;
+        std::cout << "Type name or number to call from contacts list: ";
+        std::cin >> input;
+
+        for(int i=0;i<contacts.size();i++)
+        {
+            if(contacts[i].getName()==input)
+            {
+                std::cout << "Calling " << contacts[i].getNumber() << std::endl;
+                return;
+            }
+        }
+
+        std::cout << "Calling " << input << std::endl;
+    }
+
+    void sms()
+    {
+        std::string name, text;
+        std::cout << "Type name or number to text from contacts list: ";
+        std::cin >> name;
+        std::cin.get();
+        std::cout << "Type text to send: ";
+        std::getline(std::cin, text);
+
+        for(int i=0;i<contacts.size();i++)
+        {
+            if(contacts[i].getName()==name)
+            {
+                std::cout << "Sending sms to " << contacts[i].getNumber() << std::endl;
+                std::cout << "Message: " << text << std::endl;
+                return;
+            }
+        }
+
+        std::cout << "Sending sms to " << name << std::endl;
+        std::cout << "Message: " << text << std::endl;
+    }
 };
 
 int main()
 {
-    phoneNumber Jeka("Jeka", 9213055969);
-    Jeka.addNumber(9112848784);
+    phoneList list;
+    std::string input;
 
-    std::cout << Jeka.getQty() << " " << Jeka.getNumber(0) << Jeka.getNumber(1);
+    do
+    {
+        std::cout << "Type command: ";
+        std::cin >> input;
+
+        if(input=="add")
+            list.add();
+        else if(input=="call")
+            list.call();
+        else if(input=="sms")
+            list.sms();
+    }
+    while(input!="exit");
+    return 0;
 }
